770b31441eb120f42d032efebebdbfcc10bf91ef
berheruben <rubenberhe56@gmail.com>
2021-11-09 18:12:53 +0100

    aggiungo cose

--- index.js
+++ index.js
@@ -3,23 +3,34 @@ const app = express()
 const port = 3000
 let titolo = null;
 let contatore = 0;
-
+const user =[{name:"sir",surname:"siso"}]  
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
 // calling body-parser to handle the Request Object from POST requests
 var bodyParser = require('body-parser');
 // parse application/json, basically parse incoming Request Object as a JSON Object 
 app.use(bodyParser.json());
 
 app.get('/api/orders', (req, res) => {
-    res.send('Hello World!  +  questo è il primo get')
+  res.send([{
+    id:1,
+    nome: "mario",
+    cognome:"rossi",
+    ordine:"piadina"
+  }])
 
 })
-app.get('/', (req, res) => {
-  res.send('Hello World!  +  questo è il primo get')
-
+app.get('/api/orders-by-user', (req, res) => {
+ res.send(req.body.id)
 })
-app.get('/', (req, res) => {
-  res.send('Hello World!  +  questo è il primo get')
+app.post("/", function(req,res){
+ 
+});
+app.post("/", (req, res,next,id) => {
 
+  res.send(req.body.nome)
+  res.send(req.body.cognome)
+  res.send(req.body.ordine)
 })
 /*
 app.get('/2', (req, res) => {
