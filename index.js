b8a790e7969a6be02f0d438ac8e6ab7d46de6135
berheruben <rubenberhe56@gmail.com>
2021-11-11 11:45:25 +0100

    compiti 11/11/21

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const port = 3000;
 app.use(bodyParser.json()); //il dato passato è di tipo json altrimenti non puo leggerlo
 
 let rawdata = fs.readFileSync("items.json");
-let items = JSON.parse(rawdata);//items è un array che contiene id e nome dei panini
+let items = JSON.parse(rawdata); //items è un array che contiene id e nome dei panini
 
-rawdata = fs.readFileSync("orders.json");
-let orders = JSON.parse(rawdata);//items è un array che contiene id e nome dei panini
+let data = fs.readFileSync("orders.json");
+let orders = JSON.parse(data); //items è un array che contiene id e nome dei panini
 
 //------------------------------------------------------------------------------------------------
 //resistuisce tutti gli ordini
@@ -24,55 +24,58 @@ app.get("/api/orders", (req, res) => {
 });
 
 //------------------------------------------------------------------------------------------------
-//inserisci un ordine nel array orders
+//aggiungi un ordine nel array orders
 
 app.post("/api/orders", (req, res) => {
-    const { userId, item } = req.body; // ES6 destructuring objects or arrays
-    if (!userId || !item) {
-      res.status(400).send("Specificare utente ed articolo");
+  const { userId, item } = req.body; // ES6 destructuring objects or arrays
+  if (!userId || !item) {
+    res.status(400).send("Specificare utente ed articolo");
+  }
+  const newStudent = [...orders, { userId, item }];
+  fs.writeFile("orders.json", JSON.stringify(newStudent, null, 1), (err) => {
+    //writefile scrive i dati nel file orders.JSON
+    if (err) {
+      console.log("errore", err);
+      res.status(500).send("Errore durante la scrittura del file");
     }
-    const newStudent = [...orders, { userId, item }]; 
-    fs.writeFile("orders.json", JSON.stringify(newStudent,null,1), (err) => {//writefile scrive i dati nel file orders.JSON
-        if (err) {
-          console.log("errore", err);
-          res.status(500).send("Errore durante la scrittura del file");
-        }
-        res.send({ orders: newStudent });
-      });
+    res.send({ orders: newStudent });
   });
-  
-//------------------------------------------------------------------------------------------------
-//stampa tutti  gli ordini che ha fatto un solo cliente  
-
-app.get("/api/orders/:userId", (req, res) => {
-    const { userId } = req.params;
-    if (!userId) {
-      res.status(400).send("Specifica un utente");
-    }
-    const userOrders = orders.filter((order) => {//serve a filtrare gli ordini in base al id
-      return Number(order.userId) === Number(userId); // "1" => 1
-    });
-    res.send({ orders: userOrders });
-  });
-
-//------------------------------------------------------------------------------------------------ 
-//restituisce tutti i items del file
-
-app.get("/api/orders/items", (req, res) => {
-  res.send(items);
 });
 
 //------------------------------------------------------------------------------------------------
-//è una route per aggiungere un tipo di panino nel file items.json, 
+//stampa tutti  gli ordini che ha fatto un solo cliente
+
+app.get("/api/orders/:userId", (req, res) => {
+  const { userId } = req.params;
+  if (!userId) {
+    res.status(400).send("Specifica un utente");
+  }
+  const userOrders = orders.filter((order) => {
+    //serve a filtrare gli ordini in base al id
+    return Number(order.userId) === Number(userId); // "1" => 1
+  });
+  res.send({ orders: userOrders });
+});
+
+//------------------------------------------------------------------------------------------------
+//restituisce tutti i items del file
+
+app.get("/api/items", (req, res) => {
+  res.send({ items });
+});
+
+//------------------------------------------------------------------------------------------------
+//è una route per aggiungere un tipo di panino nel file items.json,
 //quindi in questo caso sarà una post che salva nel file json
 
-app.post("/api/orders/items", (req, res) => {
-  const { id, nome } = req.body;        //prendiamo id e nome dal body
+app.post("/api/items", (req, res) => {
+  const { id, nome } = req.body; //prendiamo id e nome dal body
   if (!id || !nome) {
     res.status(400).send("Specificare utente ed articolo"); //indica un errore
   }
-    const newStudent = [...items, { id, nome }]; //ES6 destructuring array aggiunge oggetto al items 
-  fs.writeFile("items.json", JSON.stringify(newStudent,null,1), (err) => {//wriitefile scrive i dati nel file items.JSON
+  const newStudent = [...items, { id, nome }]; //ES6 destructuring array aggiunge oggetto al items
+  fs.writeFile("items.json", JSON.stringify(newStudent, null, 1), (err) => {
+    //wriitefile scrive i dati nel file items.JSON
     if (err) {
       console.log("errore", err);
       res.status(500).send("Errore durante la scrittura del file");
@@ -89,3 +92,20 @@ app.listen(port, () => {
 });
 
 //------------------------------------------------------------------------------------------------
+
+// Compiti per oggi 11/11/21
+
+// Installare sul pc Mongo (mac probabilmente usa brew)
+// Installare Robot3T (https://robomongo.org/)
+// Provare a collegarsi a mongo usando Mongoose (https://mongoosejs.com/)
+//      - npm i mongoose
+//      - mongoose.connect('mongodb://localhost:27017/paninoteka');
+//      - dentro paninoteka avrete 2 collections:
+//          - orders
+//          - items
+
+const startMongo = async () => {
+    await setTimeout(() => {});
+};
+
+startMongo();
