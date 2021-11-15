e4723a7338f281ccf5dc424ae3812eb99b2f4e46
berheruben <rubenberhe56@gmail.com>
2021-11-15 12:55:37 +0100

    Enable cors

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const { Order } = require("./models/order");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json()); //il dato passato è di tipo json altrimenti non puo leggerlo
+app.use(cors());
 
 let rawdata = fs.readFileSync("items.json");
 let items = JSON.parse(rawdata); //items è un array che contiene id e nome dei panini
